166a9bae3643be03c7661f967901928b841db141
zaighamhaider7 <zaighamhaider8@gmail.com>
2025-12-10 01:24:52 +0500

    clientS

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\project;
-use App\Models\assignTo;
+use App\Models\AssignTo;
 use Illuminate\Support\Facades\Hash;
 use App\Helpers\ActivityLogger;
 use App\Models\ActivityLog;
